5b9708790c90ba45b779a36f1d59398f56200b6d
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-14 17:00:09 +0800

    added a target link of the orders to the orders page in the customer dashboard

--- lsh_capstone/resources/views/customer/home.blade.php
+++ lsh_capstone/resources/views/customer/home.blade.php
@@ -5,19 +5,21 @@
 @section('main_content')
 <div class="row">
     <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-        <div class="card card-statistic-1">
-            <div class="card-icon bg-primary">
-                <i class="fa fa-user"></i>
-            </div>
-            <div class="card-wrap">
-                <div class="card-header">
-                    <h4>Completed Orders</h4>
+        <a href="{{ route('customer_order_view') }}">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-primary">
+                    <i class="fa fa-user"></i>
                 </div>
-                <div class="card-body">
-                    {{ $total_completed_orders }}
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Completed Orders</h4>
+                    </div>
+                    <div class="card-body">
+                        {{ $total_completed_orders }}
+                    </div>
                 </div>
             </div>
-        </div>
+        </a>
     </div>
     <div class="col-lg-4 col-md-6 col-sm-6 col-12">
         <div class="card card-statistic-1">
