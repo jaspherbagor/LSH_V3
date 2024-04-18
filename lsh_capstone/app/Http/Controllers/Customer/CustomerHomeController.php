d2946f27747adb894b4cf0def679a5d082045b92
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-18 15:25:05 +0800

    added the lists of the recent bookings in the customer dashboard

--- lsh_capstone/app/Http/Controllers/Customer/CustomerHomeController.php
+++ lsh_capstone/app/Http/Controllers/Customer/CustomerHomeController.php
@@ -13,7 +13,9 @@ class CustomerHomeController extends Controller
     {
         $total_completed_orders = Order::where('status','Completed')->where('customer_id',Auth::guard('customer')->user()->id)->count();
         $total_pending_orders = Order::where('status','Pending')->where('customer_id',Auth::guard('customer')->user()->id)->count();
-        return view('customer.home', compact('total_completed_orders','total_pending_orders'));
+
+        $recent_orders = Order::where('customer_id', Auth::guard('customer')->user()->id)->orderBy('id', 'desc')->skip(0)->take(5)->get();
+        return view('customer.home', compact('total_completed_orders','total_pending_orders', 'recent_orders'));
 
     }
 }
--- lsh_capstone/resources/views/customer/home.blade.php
+++ lsh_capstone/resources/views/customer/home.blade.php
@@ -37,4 +37,52 @@
         </div>
     </div>
 </div>
+
+<div class="row">
+    <div class="col-md-12">
+        <section class="section">
+            <div class="section-header">
+                <h1>Recent Bookings</h1>
+            </div>
+        </section>
+        <div class="section-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>SL</th>
+                                            <th>Order No</th>
+                                            <th>Payment Method</th>
+                                            <th>Booking Date</th>
+                                            <th>Paid Amount</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach($recent_orders as $row)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $row->order_no }}</td>
+                                            <td>{{ $row->payment_method }}</td>
+                                            <td>{{ \Carbon\Carbon::createFromFormat('d/m/Y', $row->booking_date)->format('F d, Y') }}</td>
+                                            <td>₱{{ number_format($row->paid_amount, 2) }}</td>
+                                            <td class="pt_10 pb_10">
+                                                <a href="{{ route('customer_invoice',$row->id) }}" class="btn btn-primary">Detail</a>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
